5bce88da82ea53f179ec34dbb7ccc39f52bd7a0a
ifedayo <adesiyanifedayo@gmail.com>
2020-12-07 05:05:29 +0100

    COnfigure redis on heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.set('trust proxy', 1);
 
